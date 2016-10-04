842c1465e34dd833a0b990a4fdf0c7063711c1d8
wimboes <wim.boes@student.kuleuven.be>
2016-10-04 10:23:10 +0200

    global, utils, output of rnn added

---
+++ Utils/config0.py
@@ -0,0 +1,12 @@
+init_scale = 0.1
+learning_rate = 1.0
+max_grad_norm = 5
+num_layers = 2
+num_steps = 20
+hidden_size = 200
+max_epoch = 4
+max_max_epoch = 13
+keep_prob = 1.0
+lr_decay = 0.5
+batch_size = 20
+vocab_size = 10000
