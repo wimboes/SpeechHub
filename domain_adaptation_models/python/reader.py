1e4bcfaee6649ef28b7a6e0a96a636c17b5906be
wimboes <wim.boes@student.kuleuven.be>
2017-03-08 09:43:08 +0100

    blabla

--- domain_adaptation_models/python/reader.py
+++ domain_adaptation_models/python/reader.py
@@ -5,18 +5,18 @@ from __future__ import division
 from __future__ import print_function
 import os
 
-from gensim import corpora, models
+from gensim import corpora
 import numpy as np
 
 ##### functions
         
-class ds_data(object):
+class ds_data_sentence(object):
     def __init__(self, batch_size, data_path, name):
         self.batch_size = batch_size
         
         #reading data
         path = os.path.join(data_path, name)
-        self.directory = os.path.join(data_path, name + '_batch_files')
+        self.directory = os.path.join(data_path, name + '_batch_files_sentence')
         info_file = os.path.join(self.directory, 'info.txt')
         if not (os.path.exists(info_file)):
             print("Creating directory %s" % self.directory)
@@ -42,6 +42,7 @@ class ds_data(object):
             else:
                 create_new_batch_files = False                     
         self._epoch_size = amount_sentences//batch_size
+        self.batch_size = batch_size
         
         #creating batch_files that will later be read
         if create_new_batch_files:
@@ -50,6 +51,7 @@ class ds_data(object):
                 os.remove(os.path.join(self.directory,f))
             with open(path, "r") as f:
                 sentences = [lines.decode('utf-8') for lines in f]
+                print(sentences)
             for i in xrange(self._epoch_size):
                 batch_file = os.path.join(self.directory, 'batch' + str(i) + '.txt')
                 with open(batch_file, 'w') as bf:
@@ -136,8 +138,131 @@ class ds_data(object):
     @property
     def epoch_size(self):
         return self._epoch_size
+        
+class ds_data_continuous(object):
+    def __init__(self, batch_size, num_steps, data_path, name):
+        self.batch_size = batch_size
+        
+        #reading data
+        path = os.path.join(data_path, name)
+        self.directory = os.path.join(data_path, name + '_batch_files_continuous')
+        info_file = os.path.join(self.directory, 'info.txt')
+        if not (os.path.exists(info_file)):
+            print("Creating directory %s" % self.directory)
+            if not (os.path.exists(self.directory)): 
+                os.mkdir(self.directory)
+            amount_words = sum(1 for line in open(path) for word in line.split())
+            self._epoch_size = (amount_words-1)//(num_steps*batch_size)
+            self.batch_size = batch_size
+            self.num_steps = num_steps
+            with open(info_file, 'w') as i_f:
+                i_f.write('batch_size: ' + str(batch_size) + '\n')
+                i_f.write('num_steps: ' + str(num_steps) + '\n')
+                i_f.write('amount_words: ' + str(amount_words) + '\n')
+            create_new_batch_files = True
+        else:
+            with open(info_file,"r") as i_f:
+                batch_size_previous = int(i_f.readline().split()[1])
+                num_steps_previous = int(i_f.readline().split()[1])
+                amount_words = int(i_f.readline().split()[1])
+            if (batch_size_previous != batch_size) or (num_steps_previous != num_steps):
+                os.remove(info_file)
+                create_new_batch_files = True
+            else:
+                create_new_batch_files = False                     
+        self._epoch_size = (amount_words-1)//(num_steps*batch_size)
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        
+        #creating batch_files that will later be read
+        if create_new_batch_files:
+            filelist = [ f for f in os.listdir(self.directory)]
+            for f in filelist:
+                os.remove(os.path.join(self.directory,f))
+            with open(path, "r") as f:
+                words = [lines.decode('utf-8').split() for lines in f]
+                words = [item for sentence in words for item in sentence]
+            for i in xrange(self._epoch_size):
+                batch_file = os.path.join(self.directory, 'batch' + str(i) + '.txt')
+                with open(batch_file, 'w') as bf:
+                    for j in xrange(batch_size):
+                        new_text_piece = words[i*num_steps+j*self._epoch_size*num_steps:(i+1)*num_steps+1+j*self._epoch_size*num_steps]
+                        print(new_text_piece)
+                        new_text_piece_decoded = ' '.join([k.encode('utf-8') for k in new_text_piece])                          
+                        bf.write(new_text_piece_decoded +'\n')
+            with open(info_file, 'w') as i_f:
+                i_f.write('batch_size: ' + str(batch_size) + '\n')
+                i_f.write('num_steps: ' + str(num_steps) + '\n')
+                i_f.write('amount_words: ' + str(amount_words) + '\n') 
+            print('creating batch_files done')
+            
+        #reading word_to_id
+        dict_path = os.path.join(data_path, "dictionary.ds.dict")
+        dictionary = corpora.Dictionary.load(dict_path)
+        self._word_to_id = dict()
+        for (wordid,word) in dictionary.iteritems():
+            self._word_to_id[word] = wordid
+        self._unk_id = self._word_to_id['<unk>']
+        self._bos_id = self._word_to_id['<bos>']
+        self._eos_id = self._word_to_id['<eos>']
+        self._pad_id = len(self._word_to_id)
+        
+        self.batch_id = 0
+        
 
-class ds_data_with_history(object):
+    def next_batch(self):
+        if self.batch_id == self.epoch_size:
+            self.batch_id = 0
+        batch_data = self.pad_id*np.ones((self.batch_size,self.num_steps))
+        batch_labels = self.pad_id*np.ones((self.batch_size,self.num_steps))
+        
+        with open(os.path.join(self.directory, 'batch' + str(self.batch_id) + '.txt'), 'r') as f:
+            sentences = [lines.decode('utf-8') for lines in f]
+        for i in xrange(self.batch_size):
+            new_sentence = sentences[i].split()
+            new_sentence = [self._word_to_id[word] for word in new_sentence]
+            
+            batch_data[i,:] = new_sentence[0:self.num_steps]
+            batch_labels[i,:] = new_sentence[1:self.num_steps+1]
+        self.batch_id += 1 
+        return batch_data, batch_labels
+        
+    def print_next_batch(self):
+        batch_data, batch_labels = self.next_batch()
+        reverse = {v: k for k, v in self._word_to_id.iteritems()}
+        reverse[self.pad_id] = 'PAD'
+        batch_lst = [reverse[int(word_id)] for sentence in batch_data for word_id in sentence]
+        targets_lst = [reverse[int(word_id)] for sentence in batch_labels for word_id in sentence]
+        for i in xrange(self.batch_size):
+            print('batch ' + str(i))
+            print(batch_lst[self.num_steps*i:self.num_steps*(i+1)])
+            print(targets_lst[self.num_steps*i:self.num_steps*(i+1)])
+        
+    @property
+    def unk_id(self):
+        return self._unk_id
+        
+    @property
+    def bos_id(self):
+        return self._bos_id
+        
+    @property
+    def eos_id(self):
+        return self._eos_id
+        
+    @property
+    def pad_id(self):
+        return self._pad_id
+
+    @property
+    def word_to_id(self):
+        return self._word_to_id
+
+    @property
+    def epoch_size(self):
+        return self._epoch_size
+
+class ds_data_sentence_with_history(object):
     def __init__(self, batch_size, history_size, data_path, name):
         self.batch_size = batch_size
 	self.history_size = history_size        
@@ -170,6 +295,7 @@ class ds_data_with_history(object):
             else:
                 create_new_batch_files = False                     
         self._epoch_size = amount_sentences//batch_size
+        self.batch_size = batch_size
         
         #creating batch_files that will later be read
         if create_new_batch_files:
