5cd561b0c8f4963c91272815c5cd7a5ba8c047f1
Wim Boes <r0385169@spchcl20.esat.kuleuven.be>
2017-03-29 18:31:56 +0200

    topics bla

---
+++ embedding_experiments/python/save_skipgram_gensim.py
@@ -0,0 +1,27 @@
+import gensim
+import os
+import numpy as np
+
+
+input_path = os.path.join(os.path.split(os.path.split(os.path.abspath(os.getcwd()))[0])[0],'input')
+filename = 'ds.train.txt'
+
+model = gensim.models.Word2Vec.load(os.path.join(os.path.split(os.path.abspath(os.getcwd()))[0],'output/embedding_skip/embedding_skip.emb')
+
+dict_path = os.path.join(input_path, "dictionary.ds")
+dictionary = gensim.corpora.Dictionary.load(dict_path)
+
+word_to_id = dict()
+for (wordid,word) in dictionary.iteritems():
+    word_to_id[word] = wordid
+
+id_to_word = {v: k for k, v in word_to_id.iteritems()}
+
+vocab_size = len(word_to_id)
+
+# +1 for padding symbol
+embedding = np.zeros([vocab_size+1,np.size(model.wv['<UNK>'])])
+for id in range(vocab_size):
+    embedding[id,:] = model.vw[id_to_word[id]]
+
+np.save('embedding.npy',embedding)
