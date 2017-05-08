5d6b7ff41d3ff049aa898063979d95f4c111346e
RobbeVR <robbe_bobbedoes@hotmail.com>
2017-05-08 21:53:42 +0200

    eee

--- untied_models/python/lambda.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash --norc
-
-compute-best-mix ../output/topic_0/interpol0.txt ../output/topic_0/interpol1.txt | grep -o "best lambda.*" | grep -o "(.*)" | grep -o "[^(].*[^)]" > lambda.txt
